3720b1d866cd2b6b0d50e430ca9f5cf091fcc1b2
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-07-28 18:04:36 +0000

    Add date, urgent & description fields to add task form

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,15 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
-    $('.collapsible').collapsible();
-    $('.tooltipped').tooltip();
+$(document).ready(function () {
+  $('.sidenav').sidenav({
+    edge: "right"
   });
+  $('.collapsible').collapsible();
+  $('.tooltipped').tooltip();
+  $('.datepicker').datepicker({
+    showClearBtn: true,
+    yearRange: 3,
+    format: "dd mmmm, yyyy",
+    i18n: {
+      done: "Select"
+    }
+  });
+});
